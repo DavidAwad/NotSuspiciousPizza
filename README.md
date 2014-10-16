d3c781fe0b806ebf16fb40c4eb90e8cfb32faefa
David <davidawad64@gmail.com>
2014-10-16 02:47:52 -0400

    Not Suspicious

---
+++ README.md
@@ -0,0 +1,82 @@
+Not Suspicious Pizza	
+============
+
+#### This is a simple model for an interactive website using flask 
+
+It also uses the twilio and sendgrid api's to send text messages and send out email!   
+just be mindful to add the api key's and credentials for both API's. 
+
+```python
+##Twilio
+account = "your api account ID here"
+token = "your live api token here"
+
+
+```
+and then the same for sendgrid
+
+```python
+##sendgrid
+sg = sendgrid.SendGridClient('your username','your id')
+```
+
+This app is meant to be an example in building a simple [Flask](http://flask.pocoo.org/) app with GETs, POSTs, and inserting and reading from an SQL database using [dataset](https://dataset.readthedocs.org/en/latest/). In addition to how you might build a website for a restaurant. 
+
+## Get your PIP (your dependency manager)
+
+### If you have a mac
+
+_if you have easy_install already, just skip to step 3, you can test by typing ```which easy_install``` in your terminal_
+
+1. Download the distribute setup script to get easy_install
+```
+http://python-distribute.org/distribute_setup.py
+```
+
+2. Run the script to install easy_install (do this in the terminal)
+```
+python distribute_setup.py
+```
+
+3. easy_install pip (do this in the terminal)
+```
+easy_install pip
+```
+
+### If you're on Windows
+
+1. Install pip-Win
+```
+https://sites.google.com/site/pydatalog/python/pip-for-windows
+```
+
+### If you're on Ubuntu
+
+1. Install with Apt (in terminal)
+```
+sudo apt-get install python-pip
+```
+
+## Install Dependencies
+
+1. In your terminal, pip install flask and dataset
+```
+pip install flask
+pip install dataset
+```
+
+## Run the server
+
+1. In your terminal, navigate to the repo's directory and run app.py
+```
+cd [REPO LOCATION]
+python app.py
+```
+
+2. Access app from localhost:5000
+
+
+## Useful Examples :
+##[Simple Flask Guest Book](https://github.com/x/Simple-Flask-Guest-Book) by [Devin Peticolas](https://github.com/x)
+##[Flask Talk f2014](https://github.com/usacs/flaskTalkF2014) by [Vaibhav Vverma](https://github.com/v)
+##
---
+++ app.py
@@ -0,0 +1,68 @@
+## Boiler Plate, api keys, imports. 
+from flask import Flask,render_template,request,redirect,url_for
+import random,sys,sendgrid,dataset
+from twilio.rest import TwilioRestClient
+account = " "
+token = " "
+client = TwilioRestClient(account, token)
+sg = sendgrid.SendGridClient('','')
+app=Flask(__name__)
+# Connnect to database
+db = dataset.connect('sqlite:///orders.db')
+# create your database of orders table
+table = db['orders']
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+	
+@app.route('/process')
+def process():
+	total = 0
+	name = request.args['name'] ##request.args will grab the name attributed from 
+	email = request.args['email']
+	number = request.args['number']
+	order = request.args['order']
+	print (order)
+	if name and number and order: ##if they enter a name and number, we can text them
+		try:		
+			###twilio sends the user a text message thanking them.
+			message = client.messages.create(to="+1 "+str(number), from_="+1 7327094438", body="Hey "+str(name)+", thanks for your order! Never fear, your "+str(order)+" pizza will be at your door as soon as our dogslayers are finished braving the cold harsh winter! #powerthrough")		
+		except:
+			print "Text message to "+name+" failed." 
+	print order
+	if order is 'Plain':
+		total = 8  ##priceboard to add scefic prices of different items to an order.
+		pass
+	elif order is 'Pepperoni':
+		total = 10
+		pass
+	elif order is 'Veggie':
+		total = 9
+		pass	
+	elif order is 'Sausage':
+		total = 11
+		pass
+	print "Email Sent"
+	###sendgrid sends an email thanking them.  
+	message=sendgrid.Mail()
+	message.add_to(str(email)) 
+	message.add_bcc('davidawad64@gmail.com') ##sends me a BCC of the email for debugging ##
+	message.set_subject("Don't worry "+str(name)+"! Pizza's on the way!") ##reassures the customer
+	message.set_html('thanks.html')
+	message.set_text('Thanks for ordering one of our delicious'+str(order)+'pizzas!')
+	message.set_from('Suspicious Pizza <Derek@pizzamail.com>')
+	status, msg = sg.send(message)
+	##create and insert mappable dictionary object into our database file. 
+	signature=dict(name=name,order=order,number=number,email=email)
+	table.insert(signature)
+	return render_template('thanks.html',total=total,order=order,email=email,number=number,name=name)
+
+# when someone sends a GET to /guest_book render guest_book.html
+@app.route('/db', methods=['GET'])
+def guest_book():
+    signatures=table.find()
+    return render_template('orderdb.html',signatures=signatures)
+
+if __name__ == '__main__':
+	app.run(debug=True)
